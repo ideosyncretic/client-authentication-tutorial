594754743657515f6eed1880f0a593b1f15d00c5
Angel Coco Chen <angel@finaxar.com>
2017-10-14 18:22:28 +0800

    Outline signin action creator

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export function signinUser ({ email, password }) {
+  // Submit email/password to server
+
+  // If request is good
+
+  // Update state to indicate user is auth'd
+  // Save the JWT
+  // Redirect to the route "/feature"
+
+  // If request is bad
+  // Show an error
+}
